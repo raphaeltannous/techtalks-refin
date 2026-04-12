d12d7bd4cff2136ecb10bac452233c5b2fd59ed4
Raphael Tannous <raphaeltannous@proton.me>
2026-04-12 15:27:43 +0300

    Create UserService class

--- app/services/user.py
+++ app/services/user.py
@@ -1,4 +1,6 @@
-from models.user import User
+import uuid
+
+from models.user import User, UserPublic, UsersPublic
 from pydantic import EmailStr
 from repositories.user import UserRepository
 
@@ -9,6 +11,23 @@ class UserService:
         user_repository: UserRepository,
     ) -> None:
         self.user_repository = user_repository
+
+    def get_public_users(self, offset: int, limit: int) -> UsersPublic:
+        users, count = self.user_repository.get_users(offset, limit)
+
+        users_public = [UserPublic.model_validate(user) for user in users]
+
+        return UsersPublic(users=users_public, count=count)
+
+    def get_private_users(self, offset: int, limit: int) -> tuple[list[User], int]:
+        users, count = self.user_repository.get_users(offset, limit)
+
+        users = [u for u in users]
+
+        return users, count
+
+    def get_by_id(self, id: uuid.UUID) -> User | None:
+        return self.user_repository.get_by_id(id)
 
     def get_by_email(self, email: EmailStr) -> User | None:
         pass
