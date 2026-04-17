8f6ab0942cc33a5cb49a68507c436bfe5a337144
Dina Amer <dinadinaamer@gmail.com>
2026-04-17 15:23:17 +0300

    Correct the code

--- app/repositories/postgres/email_verification.py
+++ app/repositories/postgres/email_verification.py
@@ -1,32 +1,47 @@
+import uuid
 from sqlmodel import Session, select
 
-from app.models.email_verification import EmailVerification, EmailVerificationUpdate
-from app.repositories.email_verification import AbstractEmailVerificationRepository
+from models.email_verification import EmailVerification, EmailVerificationUpdate
+from repositories.email_verification import EmailVerificationRepository
 
 
-class PostgresEmailVerificationRepository(AbstractEmailVerificationRepository):
+class PostgresEmailVerification(EmailVerificationRepository):
 
-    def __init__(self, session: Session) -> None:
-        self.session = session
+    def __init__(
+        self,
+        engine,
+        ) -> None:
+        self.engine = engine
 
     def get_by_token_hash(self, token_hash: str) -> EmailVerification | None:
-        statement = select(EmailVerification).where(
-            EmailVerification.token_hash == token_hash
-        )
-        return self.session.exec(statement).first()
+        with Session(self.engine) as session:
+            statement = select(EmailVerification).where(
+                EmailVerification.token_hash == token_hash
+            )
+
+            email_verification = session.exec(statement).first()
+
+            return email_verification
+
+    def add(self, email_verification_in: EmailVerification) -> EmailVerification:
+        with Session(self.engine) as session:
+            session.add(email_verification_in)
+            session.commit()
+            session.refresh(email_verification_in)
+
+            return email_verification_in
 
     def update(
         self,
         db_obj: EmailVerification,
         obj_in: EmailVerificationUpdate,
     ) -> EmailVerification:
-        update_data = obj_in.model_dump(exclude_unset=True)
+        with Session(self.engine) as session:
+            update_data = obj_in.model_dump(exclude_unset=True)
 
-        for key, value in update_data.items():
-            setattr(db_obj, key, value)
+            db_obj.sqlmodel_update(update_data)
+            session.add(db_obj)
+            session.commit()
+            session.refresh(db_obj)
 
-        self.session.add(db_obj)
-        self.session.commit()
-        self.session.refresh(db_obj)
-
-        return db_obj
+            return db_obj
