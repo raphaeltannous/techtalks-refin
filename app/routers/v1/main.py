61746646968fa7de9f4eee2eb338bcca1a1d28ba
Raphael Tannous <raphaeltannous@proton.me>
2026-04-23 22:37:07 +0300

    Implement user_skill endpoints

--- app/routers/v1/main.py
+++ app/routers/v1/main.py
@@ -2,8 +2,11 @@ from config import settings
 from fastapi import APIRouter
 
 from . import private
+from .user_profile.main import user_profile_router
 
 api_v1_router = APIRouter()
+
+api_v1_router.include_router(user_profile_router, prefix="/user-profile")
 
 if settings.ENVIRONMENT == "local":
     api_v1_router.include_router(private.router)
